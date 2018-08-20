cd8184e27a612121de9ef2b32507a974db3733d2
Marianna Ohanyan <Marianna>
2018-08-21 00:12:06 +0400

    add message and create message

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+var socket = io();
+
+socket.on('connect', function(){
+    console.log('connected to server')
+
+    socket.emit('createMessage', {
+        from:"from@gmail.com",
+        text:"message si sucseesful "
+
+    });
+});
+
+socket.on('disconnect', function(){
+    console.log('disconnected from server')
+});
+
+socket.on('newMessage', function(message){
+    console.log('new message', message)
+});
+
+
--- server/server.js
+++ server/server.js
@@ -1,16 +1,39 @@
 const path = require('path');
+const http = require('http');
+
 const express = require('express');
+const socetIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
-var app = express();
 const publicPath = path.join(__dirname, '../public')
-console.log(publicPath);
+
+var app = express();
+var server = http.createServer(app);
+var io = socetIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on("connection", (socket)=>{
+    console.log("new user");
+
+    socket.emit("newMessage",{
+        from:'xxx@gmail.com',
+        text:"absd",
+        createAt:12
+    });
+
+    socket.on("createMessage",(newMessage)=>{
+        console.log('new message',newMessage)
+    });
+
+    socket.on("disconnect",()=>{
+        console.log("user is disconnected")
+    })
+});
+
+server.listen(port,()=>{
     console.log(`server is up on ${port}`);
-})
+});
 
 
 
